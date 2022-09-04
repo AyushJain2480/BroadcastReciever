1a7b517a17200f6c09d7bc9bed6ade9c9f236ebe
Ayush <ayush@codewithme.gmail.com>
2022-09-04 18:49:16 +0530

    new commit

--- app/src/main/java/com/example/broadcast_receiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcast_receiver/MainActivity.kt
@@ -3,38 +3,46 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-/* There are 2 types of BroadCastReciever which are static and dynamic receiver
-   Static reciever are declared in the manifest and work even if your app is closed but since API level 26 most
-   broadcast can only be caught by dynamic receiver which will only work if your app is active or minimised
+/*         There are 2 types of BroadCastReciever which are static and dynamic receiver
+           Static reciever are declared in the manifest and work even if your app is closed but since API level 26 most
+           broadcast can only be caught by dynamic receiver which will only work if your app is active or minimised
+           kyuki static vale background me boht battery kha jate the
+
+            here we need to register our reciever
+            system knows we want to receive broadcastEvent in case if airplane mode changes
+
+            Useful to system to determine which app wants to recieve which intents
+            we tell the system hey we want to respond to airplanemode changes
+
+            registerReceiver()  pass BroadCastReciever and IntentFilter
+
+            we declare this dynamic reciever here because we declare intentFilter directly in our code
    */
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var receiver : AirplaneModeChangeReceiver
+    lateinit var receiver1 : BatteryReceiver
+    lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.battery_text)
 
         receiver = AirplaneModeChangeReceiver()
-
-        /*  here we need to register our reciever
-            system knows we want to receive broadcastEvent in case if airplane mode changes
-
-            Useful to system to determine which app wants to recieve which intents
-            we tell the system hey we want to respond to airplanemode changes */
-
-
-        // we declare this dynamic reciever here because we declare intentFilter directly in our code
         IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also{
-            // pass BroadCastReciever and IntentFilter
-            // instance of an AirplaneModeChangeReciever
             registerReceiver(receiver , it)
         }
-    }
 
+        receiver1 = BatteryReceiver(textView)
+        IntentFilter(Intent.ACTION_BATTERY_CHANGED).also {
+            registerReceiver(receiver1,it)
+        }
+    }
     // to avoid memory leak
     override fun onStop() {
         super.onStop()
